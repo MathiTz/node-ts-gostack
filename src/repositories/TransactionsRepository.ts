7a0a5945c20efbb3732a2bd49628b0490ee6050d
mathitz <matheusalves789@gmail.com>
2020-06-23 18:19:41 -0300

    Finish with 100%

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,14 +20,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
+    const { income, outcome } = this.transactions.reduce(
+      (acc: Balance, transaction: Transaction) => {
+        if (transaction.type === 'income') acc.income += transaction.value;
+        if (transaction.type === 'outcome') acc.outcome += transaction.value;
+
+        return acc;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+
+    const total = income - outcome;
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
     // TODO
   }
 
-  public create(): Transaction {
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
     // TODO
   }
 }
